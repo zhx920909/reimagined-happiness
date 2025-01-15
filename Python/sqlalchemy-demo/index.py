91d697ef5b82d7a6fed2cc9a00ae6a88b5ff5243
mail4Ltd <mail4Ltd@163.com>
2025-01-15 23:14:35 +0800

    feat: engine.begin() and engine.connect()

--- Python/sqlalchemy-demo/index.py
+++ Python/sqlalchemy-demo/index.py
@@ -3,6 +3,7 @@ from sqlalchemy import text
 
 engine = create_engine('mysql+pymysql://root:root@localhost:3306/demo')
 
+# 边提交边进行
 with engine.connect() as conn:
     # 创建表
     create_table_sql = """
@@ -22,3 +23,9 @@ with engine.connect() as conn:
     result = conn.execute(text('SELECT * FROM `users`'))
     for row in result:
         print(row.username, row.password)
+
+# 一次性开始
+with engine.begin() as conn:
+    # 更新数据
+    conn.execute(text('UPDATE `users` SET `password` = :password WHERE `username` = :username'),
+                 {'username': 'admin', 'password': 'admin123'})
